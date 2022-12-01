80709ca663f848cde7652d556fd015832643d374
Panna Szűcs <szucspann@gmail.com>
2022-12-01 16:08:17 +0100

    Update Job.jsx

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,20 +1,35 @@
-import { Row, Col } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import { Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { Button } from "react-bootstrap";
 
-const Job = ({ data }) => (
-  <Row
-    className="mx-0 mt-3 p-3"
-    style={{ border: '1px solid #00000033', borderRadius: 4 }}
-  >
-    <Col xs={3}>
-      <Link to={`/${data.company_name}`}>{data.company_name}</Link>
-    </Col>
-    <Col xs={9}>
-      <a href={data.url} target="_blank" rel="noreferrer">
-        {data.title}
-      </a>
-    </Col>
-  </Row>
-)
+const Job = ({ data }) => {
+  const dispatch = useDispatch();
+  return (
+    <Row
+      className="mx-0 mt-3 p-3"
+      style={{ border: "1px solid #00000033", borderRadius: 4 }}
+    >
+      <Col xs={3}>
+        <Link to={`/${data.company_name}`}>{data.company_name}</Link>
+      </Col>
+      <Col xs={9}>
+        <a href={data.url} target="_blank" rel="noreferrer">
+          {data.title}
+        </a>
+        <Button variant='info'
+          onClick={() =>
+            dispatch({
+              type: "ADD_COMPANY",
+              payload: { data },
+            })
+          }
+        >
+          Add
+        </Button>
+      </Col>
+    </Row>
+  );
+};
 
-export default Job
+export default Job;
